6885f3d2814f208d88a9f8520e21137f18c5b080
Aecrimx <mircea.alexandru.popa@gmail.com>
2025-12-09 19:17:05 +0200

    Added fullscreening and returning to windowed mode by pressing F

--- main.cpp
+++ main.cpp
@@ -17,7 +17,21 @@ SomeClass *getC() {
 }
 //////////////////////////////////////////////////////////////////////
 
+sf::View LetterboxView(sf::View view, int winWidth, int winHeight) {
+    float windowRatio = winWidth / (float) winHeight;
+    float viewRatio = view.getSize().x / (float) view.getSize().y;
+    float sizeX = 1.f, sizeY = 1.f, posX = 0.f, posY = 0.f;
 
+    if (windowRatio < viewRatio) {
+        sizeY = windowRatio / viewRatio;
+        posY = (1.f - sizeY) / 2.f;
+    } else {
+        sizeX = viewRatio / windowRatio;
+        posX = (1.f - sizeX) / 2.f;
+    }
+    view.setViewport(sf::FloatRect({posX, posY}, {sizeX, sizeY}));
+    return view;
+}
 
 
 int main() {
@@ -70,9 +84,33 @@ int main() {
     window.setVerticalSyncEnabled(true);                                    ///
     /// window.setFramerateLimit(60);                                       ///
     ///////////////////////////////////////////////////////////////////////////
-    sf::Vector2<float> pos = {100.f, 100.f};
-    sf::View view(sf::FloatRect({0.f, 0.f}, {1000.f, 1000.f}));
+
+    sf::View view(sf::FloatRect({0.f, 0.f}, {1920.f, 1080.f}));
+    //schimbarea valorilor de sus schimba rezolutia aparenta insa nu cred ca e okay asa:))
     window.setView(view);
+
+    //player stuff
+    sf::Texture texture("../test.png");
+    texture.setSmooth(true);
+    sf::Sprite sprite(texture);
+
+    //scaling sprite mai mic
+    sprite.setScale({0.2f, 0.2f});
+
+    sprite.setColor(sf::Color(0, 255, 0));
+    sprite.setOrigin({texture.getSize().x/2.f, texture.getSize().y/2.f});
+    sf::Vector2<float> PlayerPos= {100.f, 100.f};
+
+
+    sf::Texture backgroundTexture("../dirt.png");
+    backgroundTexture.setRepeated(true);
+
+    sf::RectangleShape background;
+    background.setSize({4000.f, 4000.f});  // world size (big map)
+    background.setTexture(&backgroundTexture);
+    background.setTextureRect(sf::IntRect({0, 0}, {4000, 4000}));
+
+    int toggle = 0;
     while(window.isOpen()) {
 
 
@@ -87,14 +125,25 @@ int main() {
                 std::cout << "New width: " << window.getSize().x << '\n'
                           << "New height: " << window.getSize().y << '\n';
                 const auto* resized = event->getIf<sf::Event::Resized>();
-                sf::FloatRect visibleArea({0.f, 0.f}, sf::Vector2f(resized->size));
-                window.setView(sf::View(visibleArea));
+                view = LetterboxView(view, resized->size.x, resized->size.y);
             }
             else if (event->is<sf::Event::KeyPressed>()) {
                 const auto* keyPressed = event->getIf<sf::Event::KeyPressed>();
-                std::cout << "Received key " << (keyPressed->scancode == sf::Keyboard::Scancode::X ? "X" : "(other)") << "\n";
+                //std::cout << "Received key " << (keyPressed->scancode == sf::Keyboard::Scancode::X ? "X" : "(other)") << "\n";
                 if(keyPressed->scancode == sf::Keyboard::Scancode::Escape) {
                     shouldExit = true;
+                }
+                if (keyPressed->scancode == sf::Keyboard::Scancode::F) {
+                    if (toggle == 0) {
+                        toggle = 1;
+                        window.create(sf::VideoMode({1920, 1080}), "My Window", sf::Style::Default, sf::State::Fullscreen);
+                        window.setVerticalSyncEnabled(true);
+                    }
+                    else {
+                        toggle = 0;
+                        window.create(sf::VideoMode({800, 700}), "My Window", sf::Style::Default, sf::State::Windowed);
+                        window.setVerticalSyncEnabled(true);
+                    }
                 }
             }
         }
@@ -103,28 +152,27 @@ int main() {
             std::cout << "Fereastra a fost închisă (shouldExit == true)\n";
             break;
         }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
-            pos.y -= 5.f;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) {
-            pos.y += 5.f;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
-            pos.x -= 5.f;
-        }
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
-            pos.x += 5.f;
-        }
+        const float speed = 5.f;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) PlayerPos.y -= speed;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) PlayerPos.y += speed;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) PlayerPos.x -= speed;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) PlayerPos.x += speed;
+
+
+        window.setView(view);
+        view.setCenter(PlayerPos);
+
         window.clear();
 
-        sf::Texture texture("../test.png");
-        texture.setSmooth(true);
-        sf::Sprite sprite(texture);
-        sprite.setColor(sf::Color(0, 255, 0));
 
-        sprite.setPosition(pos);
+        window.draw(background);
+
+        sprite.setPosition(PlayerPos);
         window.draw(sprite);
+
         //sf::View currentView = window.getView();
+
+
 
         window.display();
     }
