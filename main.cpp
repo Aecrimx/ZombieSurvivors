1d545372a1419dba823b1839517a212cb0b86a4e
Aecrimx <mircea.alexandru.popa@gmail.com>
2025-12-11 21:14:58 +0200

    assets+fonts directory added to repo, modified CMakeLists.txt and cmake.yml

--- main.cpp
+++ main.cpp
@@ -5,18 +5,6 @@
 
 #include <SFML/Graphics.hpp>
 
-//////////////////////////////////////////////////////////////////////
-/// This class is used to test that the memory leak checks work as expected even when using a GUI
-class SomeClass {
-public:
-    explicit SomeClass(int) {}
-};
-
-SomeClass *getC() {
-    return new SomeClass{2};
-}
-//////////////////////////////////////////////////////////////////////
-
 sf::View LetterboxView(sf::View view, int winWidth, int winHeight) {
     float windowRatio = winWidth / (float) winHeight;
     float viewRatio = view.getSize().x / (float) view.getSize().y;
@@ -35,80 +23,45 @@ sf::View LetterboxView(sf::View view, int winWidth, int winHeight) {
 
 
 int main() {
-    ///
-    std::cout << "Hello, world!\n";
-    /////////////////////////////////////////////////////////////////////////
-    /// Observație: dacă aveți nevoie să citiți date de intrare de la tastatură,
-    /// dați exemple de date de intrare folosind fișierul tastatura.txt
-    /// Trebuie să aveți în fișierul tastatura.txt suficiente date de intrare
-    /// (în formatul impus de voi) astfel încât execuția programului să se încheie.
-    /// De asemenea, trebuie să adăugați în acest fișier date de intrare
-    /// pentru cât mai multe ramuri de execuție.
-    /// Dorim să facem acest lucru pentru a automatiza testarea codului, fără să
-    /// mai pierdem timp de fiecare dată să introducem de la zero aceleași date de intrare.
-    ///
-    /// Pe GitHub Actions (bife), fișierul tastatura.txt este folosit
-    /// pentru a simula date introduse de la tastatură.
-    /// Bifele verifică dacă programul are erori de compilare, erori de memorie și memory leaks.
-    ///
-    /// Dacă nu puneți în tastatura.txt suficiente date de intrare, îmi rezerv dreptul să vă
-    /// testez codul cu ce date de intrare am chef și să nu pun notă dacă găsesc vreun bug.
-    /// Impun această cerință ca să învățați să faceți un demo și să arătați părțile din
-    /// program care merg (și să le evitați pe cele care nu merg).
-    ///
-    /////////////////////////////////////////////////////////////////////////
-
-    ///////////////////////////////////////////////////////////////////////////
-    /// Pentru date citite din fișier, NU folosiți tastatura.txt. Creați-vă voi
-    /// alt fișier propriu cu ce alt nume doriți.
-    /// Exemplu:
-    /// std::ifstream fis("date.txt");
-    /// for(int i = 0; i < nr2; ++i)
-    ///     fis >> v2[i];
-    ///
-    ///////////////////////////////////////////////////////////////////////////
-
-    SomeClass *c = getC();
-    std::cout << c << "\n";
-    delete c;  // comentarea acestui rând ar trebui să ducă la semnalarea unui mem leak
-
     sf::RenderWindow window;
-    ///////////////////////////////////////////////////////////////////////////
+
     /// NOTE: sync with env variable APP_WINDOW from .github/workflows/cmake.yml:31
-    window.create(sf::VideoMode({800, 700}), "My Window", sf::Style::Default);
-    ///////////////////////////////////////////////////////////////////////////
+    window.create(sf::VideoMode({800, 700}), "Zombie Survivors", sf::Style::Default);
     std::cout << "Fereastra a fost creată\n";
-    ///////////////////////////////////////////////////////////////////////////
-    /// NOTE: mandatory use one of vsync or FPS limit (not both)            ///
-    /// This is needed so we do not burn the GPU                            ///
-    window.setVerticalSyncEnabled(true);                                    ///
-    /// window.setFramerateLimit(60);                                       ///
-    ///////////////////////////////////////////////////////////////////////////
+    window.setVerticalSyncEnabled(true);
 
     sf::View view(sf::FloatRect({0.f, 0.f}, {1920.f, 1080.f}));
     //schimbarea valorilor de sus schimba rezolutia aparenta insa nu cred ca e okay asa:))
     window.setView(view);
 
     //player stuff
-    sf::Texture texture("../test.png");
-    texture.setSmooth(true);
+    sf::Texture texture("../assets/wizard.png");
+    texture.setSmooth(false);
     sf::Sprite sprite(texture);
 
     //scaling sprite mai mic
     sprite.setScale({0.2f, 0.2f});
 
-    sprite.setColor(sf::Color(0, 255, 0));
+    //sprite.setColor(sf::Color(0, 255, 0));
     sprite.setOrigin({texture.getSize().x/2.f, texture.getSize().y/2.f});
     sf::Vector2<float> PlayerPos= {100.f, 100.f};
 
 
-    sf::Texture backgroundTexture("../dirt.png");
+    sf::Texture backgroundTexture("../assets/grass.png");
     backgroundTexture.setRepeated(true);
 
     sf::RectangleShape background;
     background.setSize({4000.f, 4000.f});  // world size (big map)
     background.setTexture(&backgroundTexture);
-    background.setTextureRect(sf::IntRect({0, 0}, {4000, 4000}));
+    background.setTextureRect(sf::IntRect({0, 0}, {10000, 10000}));
+
+    //test text
+    sf::Font font("../fonts/game_over.ttf");
+    sf::Text text(font);
+
+    text.setString("Hello world!:)");
+    text.setCharacterSize(100);
+    text.setFillColor(sf::Color::White);
 
     int toggle = 0;
     while(window.isOpen()) {
@@ -172,7 +125,7 @@ int main() {
 
         //sf::View currentView = window.getView();
 
-
+        window.draw(text);
 
         window.display();
     }
