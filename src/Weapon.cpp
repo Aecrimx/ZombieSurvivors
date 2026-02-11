bd3ede22cb31400717a629b72e0971ec564d6111
Aecrimx <mircea.alexandru.popa@gmail.com>
2026-02-11 22:20:43 +0200

    -more tick fixing

--- src/Weapon.cpp
+++ src/Weapon.cpp
@@ -7,22 +7,24 @@
 Weapon::Weapon(const std::string &name, float cooldown, ResourceManager &res,
                float baseDmg)
     : name(name), cooldown(cooldown), timer(0.f), resources(res), level(1),
-      damage(baseDmg), baseDamage(baseDmg) {}
+      damage(baseDmg), baseDamage(baseDmg) {
+}
 
 Weapon::~Weapon() = default;
 
 Weapon *Weapon::copy() const { return clone(); }
 
-void Weapon::draw(sf::RenderWindow &window) const {}
+void Weapon::draw(sf::RenderWindow & /*window*/) const {
+}
 
-const std::string& Weapon::getName() const { return name; }
+const std::string &Weapon::getName() const { return name; }
 
 void Weapon::levelUp() {
-  if (level < 3) {
-    level++;
-    damage = baseDamage * (1.0f + (level - 1) * 0.2f); // 20% mai mult damage
-    cooldown = cooldown * 0.9f; // 10% reducere cooldown
-  }
+    if (level < 3) {
+        level++;
+        damage = baseDamage * (1.0f + (level - 1) * 0.2f); // 20% mai mult damage
+        cooldown = cooldown * 0.9f; // 10% reducere cooldown
+    }
 }
 
 bool Weapon::canLevelUp() const { return level < 3; }
@@ -32,6 +34,6 @@ int Weapon::getLevel() const { return level; }
 float Weapon::getDamage() const { return damage; }
 
 std::ostream &operator<<(std::ostream &os, const Weapon &obj) {
-  obj.print(os);
-  return os;
+    obj.print(os);
+    return os;
 }
